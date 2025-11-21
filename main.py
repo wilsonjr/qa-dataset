0b5be9c09ca29269df2d5edef87305bb84cfffb2
Wilson Marcilio-Jr <wilson_jr@outlook.com>
2025-11-21 16:38:29 -0300

    add rate-based sampling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 from datasets import Dataset, load_dataset 
+from collections import defaultdict
 
 
 def get_records_for_chatbot(dataset: Dataset):
@@ -15,17 +16,21 @@ def get_error_rate(dataset: Dataset):
 
     return dataset.to_pandas().groupby("chatbot")["is_correct"].mean()
 
-def extract_random_questions(dataset: Dataset, n: int):
-    if n > len(dataset):
-        raise ValueError(f"n must be less than or equal to the length of the dataset ({len(dataset)})")
-    if n <= 0:
-        raise ValueError(f"n must be greater than 0")
+def extract_random_questions(dataset: Dataset, sample_rate: float):
+    if sample_rate > 1 or sample_rate < 0:
+        raise ValueError(f"sample_rate must be between 0 and 1")
 
-    return dataset.select(random.sample(range(len(dataset)), n))['question']
+    buckets = defaultdict(list)
+    categories = set(dataset['category'])
+    for category in categories:
+        subset = dataset.filter(lambda x: x['category'] == category)
+        buckets[category].extend(subset.select(random.sample(range(len(subset)), int(sample_rate * len(subset)))))
+
+    return buckets
 
 if __name__ == "__main__":
     dataset = load_dataset("daloopa/financial-retrieval", split="train")
     print(get_records_for_chatbot(dataset))
     print(get_error_rate(dataset))
 
-    questions = extract_random_questions(dataset, 50)
+    questions = extract_random_questions(dataset, 0.1)
