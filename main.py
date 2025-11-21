10759f31d09b70ac7f5c2e821223f7d87dad3c45
Wilson Marcilio-Jr <wilson_jr@outlook.com>
2025-11-21 16:22:54 -0300

    select random questions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 from datasets import Dataset, load_dataset 
 
 
@@ -14,7 +15,17 @@ def get_error_rate(dataset: Dataset):
 
     return dataset.to_pandas().groupby("chatbot")["is_correct"].mean()
 
+def extract_random_questions(dataset: Dataset, n: int):
+    if n > len(dataset):
+        raise ValueError(f"n must be less than or equal to the length of the dataset ({len(dataset)})")
+    if n <= 0:
+        raise ValueError(f"n must be greater than 0")
+
+    return dataset.select(random.sample(range(len(dataset)), n))['question']
+
 if __name__ == "__main__":
     dataset = load_dataset("daloopa/financial-retrieval", split="train")
     print(get_records_for_chatbot(dataset))
     print(get_error_rate(dataset))
+
+    questions = extract_random_questions(dataset, 50)
