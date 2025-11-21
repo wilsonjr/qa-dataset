683f94c8de7ab22a50eb555a43f2e245b885f399
Wilson Marcilio-Jr <wilson_jr@outlook.com>
2025-11-21 16:20:27 -0300

    add error rate function

--- main.py
+++ main.py
@@ -7,5 +7,14 @@ def get_records_for_chatbot(dataset: Dataset):
     """
     return dataset.to_pandas()["chatbot"].value_counts()
 
-dataset = load_dataset("daloopa/financial-retrieval", split="train")
-print(get_records_for_chatbot(dataset))
+def get_error_rate(dataset: Dataset):
+    """
+    Computes the error rate based on is_correct column
+    """
+
+    return dataset.to_pandas().groupby("chatbot")["is_correct"].mean()
+
+if __name__ == "__main__":
+    dataset = load_dataset("daloopa/financial-retrieval", split="train")
+    print(get_records_for_chatbot(dataset))
+    print(get_error_rate(dataset))
